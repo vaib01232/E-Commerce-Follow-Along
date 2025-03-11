9f041ebcebd2df56e5acb39f35e79d241e283845
vaib01232 <119183394+vaib01232@users.noreply.github.com>
2025-03-11 12:36:28 +0530

    done 26

--- Backend/src/Controller/order.js
+++ Backend/src/Controller/order.js
@@ -42,4 +42,26 @@ router.post('/place-order', async (req, res) => {
     }
 });
 
+router.get('/my-orders', async (req, res) => {
+    try {
+        const { email } = req.query;
+
+        if (!email) {
+            return res.status(400).json({ message: 'Email is required.' });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found.' });
+        }
+
+        const orders = await Order.find({ user: user._id });
+
+        res.status(200).json({ orders });
+    } catch (error) {
+        console.error('Error fetching orders:', error);
+        res.status(500).json({ message: error.message});
+    }
+});
+
 module.exports = router;
