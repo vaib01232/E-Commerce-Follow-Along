09eab24ce50f216e7d3ec0e00f0916efec6a9849
vaib01232 <119183394+vaib01232@users.noreply.github.com>
2025-04-16 12:47:56 +0530

    done

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,5 +1,5 @@
 import LoginPage from "./pages/Login.jsx";
-import SignupPage from "./pages/Signup.jsx";
+import SignupPage from "./pages/signup.jsx";
 import Home from "./pages/Home.jsx";
 import CreateProduct from './pages/createProduct.jsx';
 import MyProducts from "./pages/myProduct.jsx";
