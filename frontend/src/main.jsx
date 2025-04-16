ccfbb6b681279fe4752eebcae0d99a1813b5902f
vaib01232 <119183394+vaib01232@users.noreply.github.com>
2025-04-16 12:44:21 +0530

    done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux';
-import store from './Redux/store';
+import store from './Redux/Store';
 
 import App from './App.jsx'
 
