d117ba098e5c8d39aafc8cb5ea03cf8d9396a61d
vaib01232 <119183394+vaib01232@users.noreply.github.com>
2025-04-16 12:45:52 +0530

    done

--- frontend/src/Redux/Store.js
+++ frontend/src/Redux/Store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './loginReducer';
+import userReducer from './LoginReducer';
 
 const store = configureStore({
   reducer: {
