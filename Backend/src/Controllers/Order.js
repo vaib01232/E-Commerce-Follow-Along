9aafbf9302c0b7fca6f37907b5a1e16516eff65c
Vaibhaav BS <119183394+vaib01232@users.noreply.github.com>
2025-04-16 11:36:07 +0530

    Update Order.js

--- Backend/src/Controllers/Order.js
+++ Backend/src/Controllers/Order.js
@@ -1,5 +1,5 @@
 const {Router}=require('express');
-const auth = require('../Middleware/auth');
+const auth = require('../Middleware/Auth');
 const user=require("../Model/userModel");
 const orders = require('../Model/OrderSchema');
 const rolemiddleware = require('../Middleware/role');
